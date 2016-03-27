7b6904c78785bd2a6f3b77984a15dc3d32898f76
randalx2 <pmisthry@gmail.com>
2016-03-27 20:05:56 +0200

    Completed class definition

--- Test1-Q3-2014/Time.cpp
+++ Test1-Q3-2014/Time.cpp
@@ -28,12 +28,74 @@ void Time::getTime(int &outHours, int &outMins, int &outSecs) //use reference wh
 
 void Time::setTime(int iHours, int iMins, int iSecs)
 {
-	hours = iHours;
-	mins = iMins;
-	secs = iSecs;
+	//hours = iHours;
+	//mins = iMins;
+	//secs = iSecs;
+
+	//First check for proper input
+	if (iHours > 23 || iHours < 0)
+	{
+		cout << "invalid input for hours please use correct 24hr format and enter hours now" << endl;
+		cin >> hours;
+	}
+	else
+	{
+		hours = iHours;
+	}
+
+	if (iMins > 59 || iMins < 0)
+	{
+		cout << "invalid input for minutes please use correct 24hr format and enter minutes now" << endl;
+		cin >> mins;
+	}
+	else
+	{
+		mins = iMins;
+	}
+
+	if (iSecs > 59 || iSecs < 0)
+	{
+		cout << "invalid input for seconds please use correct 24hr format and enter seconds now" << endl;
+		cin >> secs;
+	}
+	else
+	{
+		secs = iSecs;
+	}
+
 }
 
 void Time::print() const
 {
+	if (hours > 9)
+	{
+		cout << hours;
+	}
+	else
+	{
+		cout << "0" << hours;
+	}
+	cout << ":";
 
+
+	if (mins > 9)
+	{
+		cout << mins;
+	}
+	else
+	{
+		cout << "0" << mins;
+	}
+	cout << ":";
+
+	if (secs > 9)
+	{
+		cout << secs;
+	}
+	else
+	{
+		cout << "0" << secs;
+	}
+	cout << endl;
+	
 }
